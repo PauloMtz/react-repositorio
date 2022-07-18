167589120b7b7a34f093ac24d1283d724cf163f6
PauloMtz <pramosmtz@gmail.com>
2022-07-18 07:19:25 -0300

    Buscando repositório #01

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 import { FaGithub, FaPlus } from 'react-icons/fa'; // npm install react-icons
 import {Container, Form, SubmitButton} from './styles';
 
+import api from '../../services/api';
+
 export default function Home() {
+  const [newRepositorio, setNewRepositorio] = useState('');
+  const [repositorios, setRepositorios] = useState([]); // inicia com um array vazio
+
+  function handleInputChange(evento) {
+    setNewRepositorio(evento.target.value);
+    //console.log(evento.target.value);
+  }
+
+  // poderia ser essa função abaixo
+  // porém será utilizado o useCallback
+  /*async function handleSubmit(evento) {
+    evento.preventDefault();
+    //console.log(newRepositorio);
+
+    // https://api.github.com/repos/facebook/react
+    const response = await api.get(`/repos/${newRepositorio}`);
+
+    //console.log(response.data);
+
+    const dados = {
+      name: response.data.full_name,
+    }
+
+    setRepositorios([...repositorios, dados]);
+    setNewRepositorio('');
+    console.log(dados);
+  }*/
+
+  const handleSubmit = useCallback((evento) => {
+    evento.preventDefault();
+
+    async function submit() {
+      // https://api.github.com/repos/facebook/react
+      const response = await api.get(`/repos/${newRepositorio}`);
+
+      const dados = {
+        name: response.data.full_name,
+      }
+
+      setRepositorios([...repositorios, dados]);
+      setNewRepositorio('');
+      console.log(dados);
+    }
+
+    submit();
+  }, [newRepositorio, repositorios]);
+
   return(
     <Container>
       
@@ -12,8 +61,12 @@ export default function Home() {
         Meus Repositorios
       </h1>
 
-      <Form onSubmit={()=>{}}>
-        <input type="text" placeholder="Adicionar Repositorio"/>
+      <Form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Adicionar Repositorio"
+          value={newRepositorio}
+          onChange={handleInputChange} />
 
         <SubmitButton>
           <FaPlus color="#FFF" size={14}/>
